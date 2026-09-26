34c691f336ff7fd5f5edeccf89bae443f41a4700
mypetshop698-tech <mypetshop698@gmail.com>
2026-09-26 22:40:54 +0530

    qr losd fi

--- routes/receiptRoutes.js
+++ routes/receiptRoutes.js
@@ -27,33 +27,4 @@ router.post(
     sendReceiptSMS,
 );
 
-module.exports = router;const express = require('express');
-
-const {
-    createReceipt,
-    getReceipt,
-    getReceiptByOrderNo,
-} = require('../controllers/receiptController');
-
-const {
-    sendReceiptSMS,
-} = require('../controllers/smsController');
-
-const router = express.Router();
-
-router.post('/', createReceipt);
-
-// IMPORTANT: Keep this BEFORE /:token
-router.get(
-    '/order/:orderNo',
-    getReceiptByOrderNo,
-);
-
-router.get('/:token', getReceipt);
-
-router.post(
-    '/send-sms',
-    sendReceiptSMS,
-);
-
 module.exports = router;
